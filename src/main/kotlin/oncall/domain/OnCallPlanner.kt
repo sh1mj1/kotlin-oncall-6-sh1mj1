1f460e529db35244a43078f6d9cab7c60056dcf2
sh1mj1 <7wlgns@gmail.com>
2023-12-16 15:43:04 +0900

    feat(OnCallPlanner): 평일 순번과 휴일 순번으로 하나의 근무 계획을 만드는 메서드 makeWorkingPlan 구현
    아직 2일 연속으로 근무하는 결과를 재조정하지는 않았다.

---
+++ src/main/kotlin/oncall/domain/OnCallPlanner.kt
@@ -0,0 +1,35 @@
+package oncall.domain
+
+import oncall.domain.data.Date
+import oncall.domain.data.Worker
+import oncall.domain.data.WorkerSequence
+
+class OnCallPlanner(private val holidayDecider: HolidayDecider) {
+
+    fun makeWorkingPlan(date: Date, weekdayWorkers: WorkerSequence, holidayWorkers: WorkerSequence): List<Worker> {
+        val totalWorkingSequence = mutableListOf<Worker>()
+        val weekDayWork = weekdayWorkers.workers.toMutableList()
+        val holidayWork = holidayWorkers.workers.toMutableList()
+
+        var nowDate: Date = date.copy()
+        repeat(date.lastDate()) {
+            when (holidayDecider.isHoliday(nowDate)) {
+                true -> {
+                    val nextWorker = holidayWork.removeFirst()
+                    totalWorkingSequence.add(nextWorker)
+                    holidayWork.add(nextWorker)
+                }
+
+                false -> {
+                    val nextWorker = weekDayWork.removeFirst()
+                    totalWorkingSequence.add(nextWorker)
+                    weekDayWork.add(nextWorker)
+                }
+            }
+            nowDate = nowDate.nextDate()
+        }
+        return totalWorkingSequence.toList()
+    }
+
+}
+
---
+++ src/test/kotlin/oncall/domain/OnCallPlannerTest.kt
@@ -0,0 +1,32 @@
+package oncall.domain
+
+import oncall.domain.data.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class OnCallPlannerTest {
+
+    val onCallPlanner = OnCallPlanner(HolidayDecider())
+
+    @Test
+    fun `평일과 휴일 순번을 가지고 근무 순번을 만든다`() {
+        val date = Date(Month(5), 1, Week.MONDAY)
+        val weekdaySequence = WorkerSequence(
+            listOf(Worker("a"), Worker("b"), Worker("c"), Worker("d"), Worker("e"), Worker("f"))
+        )
+        val holidaySequence = WorkerSequence(
+            listOf(Worker("d"), Worker("e"), Worker("f"), Worker("a"), Worker("b"), Worker("c"))
+        )
+
+        val result = onCallPlanner.makeWorkingPlan(date, weekdaySequence, holidaySequence)
+        println(result)
+        val expected = listOf(
+            Worker("a"), Worker("b"), Worker("c"), Worker("d"), Worker("d"), Worker("e"), Worker("f"),
+            Worker("e"), Worker("f"), Worker("a"), Worker("b"), Worker("c"), Worker("a"), Worker("b"),
+            Worker("d"), Worker("e"), Worker("f"), Worker("a"), Worker("b"), Worker("c"), Worker("d"),
+            Worker("c"), Worker("d"), Worker("e"), Worker("f"), Worker("a"), Worker("e"), Worker("f"),
+            Worker("b"), Worker("c"), Worker("d"),
+        )
+        assertThat(result).isEqualTo(expected)
+    }
+}
