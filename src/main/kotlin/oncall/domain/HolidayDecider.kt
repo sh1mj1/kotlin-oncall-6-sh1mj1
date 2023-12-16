a1872bec6e81397886247e4d2bc66d026c1b2bc1
sh1mj1 <7wlgns@gmail.com>
2023-12-16 17:28:48 +0900

    feat(HolidayDecider): 날짜가 평일이면서 법정 공휴일인지 확인하는 메서드를 구현
    isWeekdayLegalHoliday 메서드

--- src/main/kotlin/oncall/domain/HolidayDecider.kt
+++ src/main/kotlin/oncall/domain/HolidayDecider.kt
@@ -2,18 +2,29 @@ package oncall.domain
 
 import oncall.domain.data.Date
 import oncall.domain.data.Holiday
+import oncall.domain.data.Month
 import oncall.domain.data.Week
 
 class HolidayDecider {
     fun isHoliday(date: Date): Boolean {
-        if (date.day == Week.SATURDAY || date.day == Week.SUNDAY) {
+        if (isWeekend(date)) {
             return true
         }
-        if (Holiday.holidays.any {
-                it.first == date.month && it.second == date.date
-            }) {
+        if (Holiday.holidays.any { isLegalHoliday(it, date) }) {
             return true
         }
         return false
     }
+
+    fun isWeekdayAndLegalHoliday(date: Date): Boolean {
+        if ((!isWeekend(date)) && (Holiday.holidays.any { isLegalHoliday(it, date) })
+        ) return true
+
+        return false
+    }
+
+    private fun isWeekend(date: Date) = date.day == Week.SATURDAY || date.day == Week.SUNDAY
+
+    private fun isLegalHoliday(it: Pair<Month, Int>, date: Date) =
+        it.first == date.month && it.second == date.date
 }
