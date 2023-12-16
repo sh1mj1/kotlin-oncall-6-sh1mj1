262ab19ede3015bd9dafdac98248d88b2ef4a572
sh1mj1 <7wlgns@gmail.com>
2023-12-16 14:30:50 +0900

    feat(Holiday): 공휴일을 저장하는 object

---
+++ src/main/kotlin/oncall/domain/data/Holiday.kt
@@ -0,0 +1,26 @@
+package oncall.domain.data
+
+object Holiday {
+    val holidays = listOf(
+        Month(1) to 1,
+        Month(3) to 1,
+        Month(5) to 5,
+        Month(6) to 6,
+        Month(8) to 15,
+        Month(10) to 3,
+        Month(10) to 9,
+        Month(12) to 25,
+    )
+}
+
+/*
+신정 the New Year 1/1
+삼일절 Independence Movement Day (March 1) 3/1
+어린이날 Children's Day 5/5
+현충일 Memorial Day 6/6
+광복절 National Liberation Day 8/15
+개천절 National foundation day of Korea 10/3
+한글날 Hangul proclamation day 10/9
+성탄절 Christmas Day 12/25
+
+ */
